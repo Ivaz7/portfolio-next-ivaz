c49b9695d1ba8842db82f11497e60738a11f1fac
Ivaz <ivazreza@gmail.com>
2025-04-16 10:07:37 +0700

    makes introduction comp responsive

--- app/component/introduction.tsx
+++ app/component/introduction.tsx
@@ -6,22 +6,23 @@ import { MapPinIcon } from "@heroicons/react/24/outline"
 
 export default function Introduction() {
   return (
-    <div className="bg-gradient-to-br from-red-900 via-red-800 to-red-700 p-6 rounded-lg shadow-xl flex flex-col gap-4 items-center text-white">
-      <Image 
-        src="/profile.jpg"
-        alt="profile picture of Ivaz Reza"
-        width={100}
-        height={100}
-        className="rounded-full border-4 border-white shadow-md"
-      />
+    <div className="bg-gradient-to-br from-red-900 via-red-800 to-red-700 p-4 sm:p-5 md:p-6 rounded-lg shadow-xl flex flex-row md:flex-col justify-center gap-4 items-center text-white">
+      <div className="relative w-[70px] h-[70px] md:w-[100px] md:h-[100px] shrink-0">
+        <Image 
+          src="/profile.jpg"
+          alt="profile picture of Ivaz Reza"
+          fill
+          className="rounded-full border-4 border-white shadow-md"
+        />
+      </div>
       <div className="flex flex-col">
-        <p className={clsx("text-xl font-bold tracking-wide text-center", poppins.className)}>
+        <p className={clsx("text-base sm:text-lg md:text-xl font-bold tracking-wide text-center", poppins.className)}>
           Hi, I&apos;m M. Ivaz Reza H.
         </p>
-        <p className="text-md font-medium text-center text-zinc-300">
+        <p className="text-sm sm:text-base md:text-lg font-medium text-center text-zinc-300">
           I&apos;m Front End Web Dev
         </p>
-        <p className="text-md font-medium text-zinc-300 flex flex-row gap-0.5 justify-center items-center">
+        <div className="text-sm sm:text-base md:text-lg font-medium text-center text-zinc-300 flex flex-row gap-0.5 justify-center items-center">
           <MapPinIcon className="w-4 h-4 text-zinc-300" />
           Cirebon, Indonesia 
           <Flag 
@@ -31,7 +32,7 @@ export default function Introduction() {
               height: "fit-content" 
             }} 
             className="border border-zinc-300" />
-        </p>
+        </div>
       </div>
     </div>
   )
