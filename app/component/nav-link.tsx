bdcd69adc31ef5ddb888b8e42e220e9d9ecac52e
Ivaz <ivazreza@gmail.com>
2025-04-16 10:54:06 +0700

    makes the style for flexible for navlinks

--- app/component/nav-link.tsx
+++ app/component/nav-link.tsx
@@ -43,16 +43,23 @@ export default function Navlink({
             key={inx}
             href={val.href}
             className={clsx(
-              "flex grow-1 flex-row gap-2 justify-center md:justify-start items-center p-3 rounded-lg hover:bg-stone-900 hover:text-red-700",
+              "flex grow-1 flex-row gap-2 justify-center md:justify-start items-center p-3 rounded-lg hover:bg-stone-800 sm:hover:bg-stone-900 hover:text-red-700",
+              title 
+                ? "hover:bg-stone-800"
+                : "hover:bg-stone-900",
               pathname === val.href 
-                ? "bg-stone-900 text-red-700"
-                : ""
+                ? "sm:bg-stone-900 text-red-700 bg-stone-800"
+                : title
+                  ? "sm:bg-transparent bg-stone-900"
+                  : "bg-transparent"
             )}
           >
             <LinkIcon className="w-10 h-10" />
-            {title && <p className="text-3xl hidden sm:block">
-              {val.name}
-            </p>}
+            {title && 
+              <p className="text-3xl hidden sm:block">
+                {val.name}
+              </p>
+            }
           </Link>)
       })}
     </>
